b2f29109b4ff6fd61d1c8ac6c70ca91637a41202
milezperhour <hyperbolvoid@gmail.com>
2020-06-09 19:53:11 -0700

    mouseOver and mouseOut work

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../css/portfolio.css';
 import dataAndAi from '../images/portfolio/data-and-ai.jpg';
 import deviceViewer from '../images/portfolio/device-viewer.jpg';
@@ -13,23 +13,45 @@ const Portfolio = () => {
   // const [isHovered, setIsHovered] = useState(false);
 
   const portfolioItems = [
-    { name: 'Portal', src: portal },
-    { name: 'Codazen Labs', src: codazenLabs },
+    {
+      name: 'Portal',
+      src: portal,
+      description:
+        'A Tron-inspired tic-tac-toe game made by me with a team of three. I served as as UI/UX Designer and Frontend Developer.',
+      gitHubLink: 'https://github.com/milezperhour/C11.16_tictactoe',
+      liveLink: 'https://milezperhour.github.io/C11.16_tictactoe/',
+    },
+    { name: 'Codazen Labs Blob Animations', src: codazenLabs },
     { name: 'Data & AI Landing Page', src: dataAndAi },
     { name: 'Unika Device Viewer', src: deviceViewer },
     { name: 'Rolls-Royce Landing Page', src: cullinan },
     { name: 'Calculator', src: calculator },
     { name: 'VenueFind', src: venueFind },
-    { name: 'ticTacToe', src: ticTacToe },
+    { name: 'Tic-Tac-Toe', src: ticTacToe },
   ];
 
-  const handleMouseOver = () => {
-    console.log('test');
+  const list = [...Array(portfolioItems.length).keys()];
+  const itemsRef = useRef(list);
+
+  const handleMouseOver = idx => e => {
+    const next = itemsRef.current[idx];
+    if (next) {
+      const imgUrl = e.target.style.backgroundImage;
+      e.target.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), ${imgUrl}`;
+    }
   };
 
-  const handleMouseOut = () => {
-    console.log('test');
-    // `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), ${imgUrl}`;
+  const handleMouseOut = idx => e => {
+    const next = itemsRef.current[idx];
+    if (next) {
+      const overlayImg = e.target.style.backgroundImage;
+      const imgUrl = overlayImg
+        .split(
+          'linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), ',
+        )
+        .pop();
+      e.target.style.backgroundImage = imgUrl;
+    }
   };
 
   const PortfolioItem = props => {
@@ -41,6 +63,7 @@ const Portfolio = () => {
         onBlur={() => undefined}
         onMouseOver={props.mouseOver}
         onFocus={() => undefined}
+        ref={props.reference}
       />
     );
   };
@@ -56,6 +79,9 @@ const Portfolio = () => {
               src={item.src}
               mouseOut={handleMouseOut(i)}
               mouseOver={handleMouseOver(i)}
+              reference={el => {
+                itemsRef.current[i] = el;
+              }}
             />
           </div>
         ))}
